6afd2becc7e43b10115d6e7b435a60c9ac598765
unknown <KefouegFrank>
2024-03-28 17:28:16 +0100

    project and task table creat

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +17,15 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'frank',
+            'email' => 'frank@example.com',
+            'password' => bcrypt('frank123'),
+            'email_verified_at' => time()
         ]);
+
+        Project::factory()
+            ->count(20)
+            ->hasTasks(20)
+            ->create();
     }
 }
