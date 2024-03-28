26539063b0d33bc94ec9517544244a51e0ffcec3
unknown <KefouegFrank>
2024-03-28 17:42:20 +0100

    project and task table creat

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -27,8 +27,8 @@ class TaskFactory extends Factory
             'created_by' => 1,
             'updated_by' => 1,
             'project_id' => 1,
-            'created_at' => now(),
-            'updated_at' => now(),
+            'created_at' => time(),
+            'updated_at' => time(),
         ];
     }
 }
