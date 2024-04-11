ea135b618c07bf9f74c13b77a9fe460cb7d0d9eb
unknown <KefouegFrank>
2024-04-11 17:18:11 +0200

    project create view added

--- resources/js/Pages/Project/index.jsx
+++ resources/js/Pages/Project/index.jsx
@@ -6,7 +6,7 @@ import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants";
 import { Head, Link, router } from "@inertiajs/react";
 import TableHeading from "@/Components/TableHeading";
 
-export default function index({ auth, projects, queryParams = null }) {
+export default function index({ auth, projects, queryParams = null, success }) {
   queryParams = queryParams || {};
   const searchFieldChanged = (name, value) => {
     if (value) {
@@ -48,7 +48,10 @@ export default function index({ auth, projects, queryParams = null }) {
           <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             Projects
           </h2>
-          <Link href={route('project.create')} className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600 ml-2">
+          <Link
+            href={route("project.create")}
+            className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600 ml-2"
+          >
             Add new
           </Link>
         </div>
@@ -58,6 +61,13 @@ export default function index({ auth, projects, queryParams = null }) {
 
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          {/* displaying success message */}
+          {success && (
+            <div className="bg-emerald-500 py-2 px-4 text-white rounded mb-4">
+              {success}
+            </div>
+          )}
+
           <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div className="p-6 text-gray-900 dark:text-gray-100">
               <div className="overflow-auto ">
